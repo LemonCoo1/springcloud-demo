f227ece8c928d4f97d64f76692a4b19f0c4e6e50
Xucong <xcong94@163.com>
2018-11-26 01:11:07 +0800

    add monitor-hystrix-dashboard module

--- monitor-admin/src/main/java/com/xc/monitoradmin/MonitorAdminApplication.java
+++ monitor-admin/src/main/java/com/xc/monitoradmin/MonitorAdminApplication.java
@@ -2,18 +2,12 @@ package com.xc.monitoradmin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.Configuration;
 
 @EnableAdminServer
 @EnableDiscoveryClient
-@EnableHystrix
-@EnableHystrixDashboard
 @SpringBootApplication
 public class MonitorAdminApplication {
 
